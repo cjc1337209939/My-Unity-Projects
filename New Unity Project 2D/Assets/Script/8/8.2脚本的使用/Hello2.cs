37c39eda31fdc8ef1a77c970c9242401f07544ca
cjc <1337209939@qq.com>
2021-05-07 16:15:36 +0800

    8.3，复习在此结束，接下来学新知识

--- New Unity Project 2D/Assets/Script/8/8.2脚本的使用/Hello2.cs
+++ New Unity Project 2D/Assets/Script/8/8.2脚本的使用/Hello2.cs
@@ -7,7 +7,7 @@ public class Hello2 : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
-        
+        Debug.Log("这是我的第一个游戏");
     }
 
     // Update is called once per frame
